ac15508650cd5597ac11990cce2aedca152b09f9
Zain <zainkazi27@gmail.com>
2024-01-01 11:36:44 +0500

    completed CreateTimeSlot page

--- appointment-frontend/src/CreateTimeSlot.jsx
+++ appointment-frontend/src/CreateTimeSlot.jsx
@@ -1,5 +1,55 @@
+import { useFormik } from "formik";
+import { useNavigate } from "react-router-dom";
+
 const CreateTimeSlot = () => {
-  return <div>Create slot</div>;
+  const navigate = useNavigate();
+
+  const formik = useFormik({
+    initialValues: {
+      startTime: "",
+      endTime: "",
+      isBooked: false,
+    },
+    onSubmit: async (values) => {
+      await fetch("http://localhost:3000/api/slots", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(values),
+      });
+      navigate("/");
+    },
+  });
+
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <label>Start Time</label>
+      <input
+        type="text"
+        id="startTime"
+        name="startTime"
+        className="border border-black"
+        onChange={formik.handleChange}
+        value={formik.values.startTime}
+      />
+      <label>End Time</label>
+      <input
+        type="text"
+        id="endTime"
+        name="endTime"
+        className="border border-black"
+        onChange={formik.handleChange}
+        value={formik.values.endTime}
+      />
+      <button
+        type="submit"
+        className="p-2 text-lg bg-gray-400 border-2 border-black"
+      >
+        Create Slot
+      </button>
+    </form>
+  );
 };
 
 export default CreateTimeSlot;
