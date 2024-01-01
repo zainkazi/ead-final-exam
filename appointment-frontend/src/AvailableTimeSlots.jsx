b838cdface452cba478974248a64fe29ae714629
Zain <zainkazi27@gmail.com>
2024-01-01 11:41:32 +0500

    completed AvailableTimeSlots page

--- appointment-frontend/src/AvailableTimeSlots.jsx
+++ appointment-frontend/src/AvailableTimeSlots.jsx
@@ -1,5 +1,33 @@
+import { useState, useEffect } from "react";
+
 const AvailableTimeSlots = () => {
-  return <div>Available slots</div>;
+  const [slots, setSlots] = useState([]);
+
+  useEffect(() => {
+    const getSlots = async () => {
+      const data = await fetch("http://localhost:3000/api/slots");
+      const results = await data.json();
+      setSlots(results.slots);
+    };
+
+    getSlots();
+  }, []);
+
+  return (
+    <div>
+      <h1>Booked Appointments</h1>
+      {slots.map((slot) => (
+        <div
+          key={slot._id}
+          className="flex items-center justify-start space-x-10"
+        >
+          <p>
+            {slot.startTime} - {slot.endTime}
+          </p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default AvailableTimeSlots;
